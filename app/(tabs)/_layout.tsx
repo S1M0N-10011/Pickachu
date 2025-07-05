b08dfecf2f6bec207c6faa334f9fe06fc7b17e39
Niels Groenewegen <134908815+onverklaarbaar@users.noreply.github.com>
2025-07-05 11:00:37 +0200

    Very basic navigation

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,41 @@
+import { Tabs } from 'expo-router';
+
+import Ionicons from '@expo/vector-icons/Ionicons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+
+export default function TabLayout() {
+  return (
+      <Tabs
+        screenOptions={{
+          tabBarActiveTintColor: '#ffd33d',
+          headerStyle: {
+            backgroundColor: '#25292e',
+          },
+          headerShadowVisible: false,
+          headerTintColor: '#fff',
+          tabBarStyle: {
+            backgroundColor: '#25292e',
+          },
+        }}
+      >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, focused }) => (
+            <Ionicons name={focused ? 'home-sharp' : 'home-outline'} color={color} size={24} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          title: 'Account',
+          tabBarIcon: ({ color, focused }) => (
+            <MaterialCommunityIcons name={focused ? 'account-circle' : 'account-circle-outline'} color={color} size={24}/>
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
